63ffbdc43273e8fe972a3cf1528c5f0ce134e2a5
tjfv02 <tjfv02@gmail.com>
2020-05-08 18:51:05 -0600

    Agregar archivos de proyecto.

---
+++ ED1_ProyectoCOVID/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ED1_ProyectoCOVID
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ED1_ProyectoCOVID/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ED1_ProyectoCOVID.MvcApplication" Language="C#" %>
